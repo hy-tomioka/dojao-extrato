de748b00bb7d770cfc8c1346bee0b010761410e2
yudi <hy-tomioka@github.com>
2021-10-05 10:28:16 -0300

    ajustes de pastas

---
+++ src/main/java/br/com/zup/extrato/ExtratoApplication.java
@@ -0,0 +1,13 @@
+package br.com.zup.extrato;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExtratoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExtratoApplication.class, args);
+	}
+
+}
